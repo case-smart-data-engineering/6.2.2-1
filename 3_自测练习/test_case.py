fe377c146f0e6da6b495a2d82633a61610c07a30
1327016969 <77666948+1327016969@users.noreply.github.com>
2023-01-30 16:27:01 +0800

    Update test_case.py

--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -6,9 +6,9 @@ from my_solution import get_probability
 
 # 测试用例
 
-os.system('my_solution.py')
+os.system('3_自测练习/my_solution.py')
 with open("3_自测练习/cookbook_test.txt", "r", encoding="utf-8") as f:
     articles = f.readlines()
 
-# 加入了参数为0.2的拉普拉斯平滑处理后，’青椒炒肉片‘出现的概率应为0.101
+# 加入了参数为0.2的拉普拉斯平滑处理后，’青椒炒肉片‘出现的概率保留三位小数应为0.101
 assert round(get_probability('青椒炒肉片'), 3) == 0.101
