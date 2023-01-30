b0ba22f1d2661ce592608a9020bbf21dc45a73bd
1327016969 <77666948+1327016969@users.noreply.github.com>
2023-01-30 16:03:41 +0800

    Update test_case.py

--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -7,7 +7,7 @@ from my_solution import get_probability
 # 测试用例
 
 os.system('my_solution.py')
-with open("cookbook_test.txt", "r", encoding="utf-8") as f:
+with open("3_自测练习/cookbook_test.txt", "r", encoding="utf-8") as f:
     articles = f.readlines()
 
 # 加入了参数为0.2的拉普拉斯平滑处理后，’青椒炒肉片‘出现的概率应为0.101
